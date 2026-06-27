0bf3ed19da69acad98af3003a7d9a36b8ee6ac45
Chelsia Gladwin <154498547+chelsiagladwin@users.noreply.github.com>
2026-06-27 13:31:53 +0530

    Add EXPLAIN to SQL query for performance analysis
    Added an EXPLAIN statement to analyze query performance and included observations about the execution plan.

--- Module3_DatabaseIntegration-Chelsia/Hands_On_4/task1.sql
+++ Module3_DatabaseIntegration-Chelsia/Hands_On_4/task1.sql
@@ -1,1 +1,13 @@
+EXPLAIN
+SELECT 
+  s.first_name, 
+  s.last_name, 
+  c.course_name
+FROM enrollments e
+JOIN students s ON s.student_id = e.student_id
+JOIN courses c ON c.course_id = e.course_id
+WHERE s.enrollment_year = 2022;
 
+-- Observation:
+-- Query examined the students, enrollments and courses tables.
+-- MySQL performed a Full Table Scan (type = ALL) because no indexes were available on the filtering column enrollment_year.
